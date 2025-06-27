9e123435d04aa2220c1bcb817872ada3387bfd8e
MD.Abu Ayub Bhuyan <abuayubminhaz290@gmail.com>
2025-06-27 22:49:40 +0600

    changer the server code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;  // Render uses its own port!
 
 const server = http.createServer((req, res) => {
     const filePath = path.join(__dirname, "index.html");
